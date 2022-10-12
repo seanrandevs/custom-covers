b66709d0f5cfa311ccc10f18e2c211665a218fd1
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2022-10-11 19:30:42 -0500

    Finished the navbar.
    Starting on the home page

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import { VscChromeClose } from "react-icons/vsc";
+import { GoThreeBars }from "react-icons/go";
+import { useRef } from "react";
+
+const Header = () => {
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
+  return (
+   <section className="navbar navbar-fixed-top navbar-expand custom-navbar">
+    <div className="navbar-brand">CUSTOM COVERS</div>
+      <nav ref={navRef} className="collapse navbar-collapse">
+        <ul>
+          <li>WORK</li>
+          <li>ABOUT</li>
+          <li>GALLERY</li>
+          <li>CONTACT</li>
+        </ul>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <VscChromeClose size={30} />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <GoThreeBars size={30} />
+      </button>
+    </section>
+  )
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,62 @@
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="site-slider">
+        <ul className="bxslider">
+            <li>
+                <img src="" alt="" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="slider-caption">
+                                <h2>GET YOUR CUSTOM COVERS</h2>
+                                <p class="color-white">Best in the business</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <img src="" alt="" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Home
