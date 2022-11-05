1dbee91fc32269350ce543b042e092d061771c9e
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2022-11-05 16:31:06 -0500

    Finished styling the form section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,7 @@
 
-
 const Contact = () => {
   return (
     <div className="contact">
-
     <div className="parallax-section-about">
   		<div className="section-title">
   			<h5>PLACE AN ORDER</h5>
@@ -12,29 +10,24 @@ const Contact = () => {
   			<p>If you have any questions or want to place and order, just shoot us a message and we will get back to you shortly.</p>
   		</div>
 	</div>
-    
-      <div className="col-md-offset-1 col-md-10 col-sm-12">
-        <form action="">
-            <div className="col-md-4 col-sm-6">
-                <input type="text" className="form-control" placeholder="Name" name="name" />
-            </div>
-            <div className="col-md-4 col-sm-6">
-                <input type="email" className="form-control" placeholder="Email" name="email" />
-            </div>
-            <div className="col-md-4 col-sm-6">
-                <input type="text" className="form-control" placeholder="Subject" name="subject" />
-            </div>
-            <div className="col-md-12 col-sm-12">
-                <textarea className="form-control" placeholder="Message" name="message" rows="7"></textarea>
-            </div>
-            <div className="col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8">
-                <input type="submit" class="form-control" value="Send Message" />
-            </div>
-        </form>
-      </div>
-
-
-
+     
+	<form>
+		<div className="form-group row">
+			<div className="col-md-4 col-sm-12">
+				<input type="text" class="form-control" placeholder="Name" />
+				</div>
+				<div className="col-md-4 col-sm-12">
+				<input type="email" class="form-control" placeholder="Email" />
+				</div>
+				<div className="col-md-4 col-sm-12">
+				<input type="text" class="form-control" placeholder="Subject" />
+			</div>
+		</div>
+				<textarea name="" className="form-control" placeholder="Message" cols="100" rows="7"></textarea>
+				<div className="submit-btn">
+					<input type="submit" class="form-control" value="SEND MESSAGE" />
+				</div>
+	</form>
 
     </div>
   )
